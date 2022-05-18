179788131d727aa0e1954225916504a84ff1f8d7
Tuna523 <duya1010@naver.com>
2022-05-18 10:19:33 +0900

    코드 추가

--- src/Chat.js
+++ src/Chat.js
@@ -3,11 +3,22 @@ import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useState,useEffect } from 'react'
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
 import MicIcon from "@material-ui/icons/Mic"
+import { useParams } from "react-router-dom";
 import "./Chat.css"
+import db from "./firebase";
 
 function Chat() {
 const [input, setInput] = useState("");
 const [seed, setSeed] = useState("");
+const { roomId } = useParams();
+const {roomName, setRoomName} = useState("");
+
+useEffect(() => {
+    if (roomId) {
+        db.collection('rooms').doc(roomId).onSnapshot((snapshot) =>
+            setRoomName(snapshot.data().name));
+    }
+}, [roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,7 +1,8 @@
 import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
-// import db from "./firebase"
+import db from "./firebase"
+import { Link } from "react-router-dom"
 
 function SidebarChat({ id, name, addNewChat }) {
     const [seed, setSeed] = useState("");
@@ -11,21 +12,26 @@ function SidebarChat({ id, name, addNewChat }) {
     }, []);
 
     const createChat = () => {
-        const roomName = prompt("Please enter name for chat");
+        const roomName = prompt("Please enter name for chat room");
 
         if (roomName){
             // something clever database stuff here!!
+            db.collection('rooms').add({
+                name: roomName,
+            });
         }
     };
 
   return !addNewChat ? (
-    <div className='sidebarChat'>
-        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-        <div className="sidebarChat__info">
+      <Link to={`/rooms/${id}`}>
+            <div className='sidebarChat'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
             <h2>{name}</h2>
             <p>Last message...</p>
+            </div>
         </div>
-    </div>
+    </Link>
   ): (
       <div onClick={createChat} className="sidebarChat">
         <h2>Add new Chat</h2>
